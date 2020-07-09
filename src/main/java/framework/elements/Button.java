da37919b7d22b1da028d8ebc50aeb5e1ef09b57d
Louismart <megapolice78@gmail.com>
2020-07-09 22:52:25 +0200

    Button, Row, TextLabelElement classes creation

--- src/main/java/framework/elements/Button.java
+++ src/main/java/framework/elements/Button.java
@@ -1,4 +1,19 @@
 package framework.elements;
 
-public class Button {
+import framework.driver.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class Button extends BaseElement {
+
+    public Button(BaseDriver baseDriver, WebElement webElement, By locator) {
+    super(baseDriver, webElement, locator);
+}
+    public void click()
+    {
+        baseDriver.getBaseWaiter().getWait()
+                .until(ExpectedConditions.elementToBeClickable(webElement))
+                .click();
+    }
 }
--- src/main/java/framework/elements/TextLabelElement.java
+++ src/main/java/framework/elements/TextLabelElement.java
@@ -1,4 +1,12 @@
 package framework.elements;
 
-public class TextLabelElement {
+import framework.driver.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TextLabelElement extends BaseElement {
+    public TextLabelElement(BaseDriver baseDriver, WebElement webElement, By locator) {
+        super(baseDriver, webElement, locator);
+
+    }
 }

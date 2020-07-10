b0c60da5ddf0b0d0d39a957fb4c2ab4035834936
Louismart <megapolice78@gmail.com>
2020-07-10 09:17:37 +0200

    PageDropDown, SimpleCheckbox, CustomerStartPage classes creation

--- src/main/java/Model/Pages/CustomerStartPage.java
+++ src/main/java/Model/Pages/CustomerStartPage.java
@@ -1,4 +1,43 @@
 package Model.Pages;
 
-public class CustomerStartPage {
+import framework.driver.BaseDriver;
+import framework.elements.*;
+import framework.pages.BaseStartPage;
+import org.openqa.selenium.By;
+
+public class CustomerStartPage extends BaseStartPage {
+    private static By searchFieldLocator = By.id("search-input");
+
+    private static By searchDropDownLocator = By.id("search-column");
+
+    private static By matchCaseCheckBoxLocator = By.id("match-case");
+
+    private static By customerTableLocator = By.xpath("//table[@class = 'table table-hover']");
+
+    private static By tableResumeLocator = By.id("table-resume");
+
+    public CustomerStartPage(BaseDriver baseDriver, String link) {
+        super(baseDriver, link);
+    }
+
+    public TextInputString search() {
+        return element(TextInputString.class, searchFieldLocator);
+    }
+
+    public PageDropDown dropDown() {
+        return element(PageDropDown.class, searchDropDownLocator);
+    }
+
+    public SimpleCheckbox checkbox() {
+        return element(SimpleCheckbox.class, matchCaseCheckBoxLocator);
+    }
+
+    public Table customerTable() {
+        return element(Table.class, customerTableLocator);
+    }
+
+    public TextLabelElement tableResume() {
+        return element(TextLabelElement.class,  tableResumeLocator);
+    }
+
 }
--- src/main/java/framework/elements/SimpleCheckbox.java
+++ src/main/java/framework/elements/SimpleCheckbox.java
@@ -1,4 +1,24 @@
 package framework.elements;
 
-public class SimpleCheckbox {
+import framework.driver.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class SimpleCheckbox extends BaseElement {
+
+    public SimpleCheckbox(BaseDriver baseDriver, WebElement webElement, By locator) {
+        super(baseDriver, webElement, locator);
+    }
+
+    public void check(){
+        if(!webElement.isSelected()){
+            webElement.click();
+        }
+    }
+
+    public void uncheck(){
+        if(webElement.isSelected()){
+            webElement.click();
+        }
+    }
 }

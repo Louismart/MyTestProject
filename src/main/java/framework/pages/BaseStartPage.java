404bc39518eee4b5b31b726076646476b7140f88
Louismart <megapolice78@gmail.com>
2020-07-10 08:17:48 +0200

    BasePage, BaseStartPage, classes creation

--- src/main/java/framework/pages/BaseStartPage.java
+++ src/main/java/framework/pages/BaseStartPage.java
@@ -1,4 +1,17 @@
 package framework.pages;
 
-public class BaseStartPage {
+import framework.driver.BaseDriver;
+
+public abstract class BaseStartPage extends BasePage {
+
+    private String link;
+
+    public BaseStartPage(BaseDriver baseDriver, String link) {
+        super(baseDriver);
+        this.link = link;
+    }
+
+    public void openUrl(){
+        getBaseDriver().getWebDriver().navigate().to(link);
+    }
 }

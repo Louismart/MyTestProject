8a58a3f95c8f05345a2a2f3f345665b38b86c859
Louismart <megapolice78@gmail.com>
2020-07-10 08:46:37 +0200

    TexInputString, Table classes creation

--- src/main/java/framework/elements/Table.java
+++ src/main/java/framework/elements/Table.java
@@ -1,4 +1,20 @@
 package framework.elements;
 
-public class Table {
+import framework.driver.BaseDriver;
+import framework.elements.tableElements.Row;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Table extends BaseElement {
+
+    public static By rowsLocator = By.xpath("tbody/tr");
+
+    public Table(BaseDriver baseDriver, WebElement webElement, By locator) {
+        super(baseDriver, webElement, locator);
+    }
+    public List<Row> rows() {
+        return findElements(Row.class, rowsLocator);
+    }
 }
--- src/main/java/framework/elements/TextInputString.java
+++ src/main/java/framework/elements/TextInputString.java
@@ -1,4 +1,22 @@
 package framework.elements;
 
-public class TextInputString {
+import framework.driver.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TextInputString extends BaseElement {
+
+    public TextInputString(BaseDriver baseDriver, WebElement webElement, By locator) {
+        super(baseDriver, webElement, locator);
+    }
+
+    public TextInputString sendString(CharSequence... charSequences) {
+        webElement.sendKeys(charSequences);
+        return this;
+    }
+
+    public TextInputString clearString() {
+        webElement.clear();
+        return this;
+    }
 }

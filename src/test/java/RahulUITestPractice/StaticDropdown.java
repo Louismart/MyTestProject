d4d340274e384837c9e397452833595e5dda06db
Louismart <megapolice78@gmail.com>
2021-01-05 21:43:09 +0100

    UI Rahul Sheety Test- suggested dropdown

--- src/test/java/RahulUITestPractice/StaticDropdown.java
+++ src/test/java/RahulUITestPractice/StaticDropdown.java
@@ -10,6 +10,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static automationTestFramework.Variables.getUrl;
 
 public class StaticDropdown {
@@ -35,6 +37,27 @@ public class StaticDropdown {
         String dropdownValue = dropdown.getFirstSelectedOption().getText();
 
         Assert.assertTrue(dropdownValue.contains(searchValue));
+
+    }
+
+    @Test
+    public void testSuggestedDropdown() throws InterruptedException {
+
+        driver.findElement(By.id("autosuggest")).sendKeys("ind");
+        Thread.sleep(2000);
+        String genericXpathForInd = "li[class='ui-menu-item'] a";
+        List<WebElement> options = driver.findElements(By.cssSelector(genericXpathForInd)); //generic XPATH for all countries start from Ind
+        String searchRequest = "India";
+        //Store all Ind elements in List
+        for (WebElement option : options) {
+         if(option.getText().equalsIgnoreCase(searchRequest))  // select India and click
+         {
+             option.click();
+             break;
+         }
+        }
+
+        //Assert.assertTrue(options.contains(genericXpathForInd));
 
     }
 
@@ -68,7 +91,8 @@ public class StaticDropdown {
         driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
         driver.findElement(By.xpath("//a[@value='BLR']")).click(); //Selenium select by default first element from the left "FROM"
         Thread.sleep(1000);
-        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click(); // point out to second element in "TO"
+        //driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click(); incorrect XPATH practise   // point out to second element in "TO"
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click(); //XPATH with parent indication
 
 
     }
